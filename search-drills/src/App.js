79dc03fcf3f4a33cd463cc7ba8a5cd707c47bcd8
gdreid13 <gdreid13@gmail.com>
2020-09-05 15:11:06 -0700

    first commit

---
+++ search-drills/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactAlgorithm from './ReactAlgorithm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactAlgorithm />
+    </div>
+  );
+}
+
+export default App;
